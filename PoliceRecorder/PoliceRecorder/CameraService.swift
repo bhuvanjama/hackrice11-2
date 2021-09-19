1192d30719cd338e0305a42923930e0560911e5a
Pranav Dulepet <ps.dulepet@gmail.com>
2021-09-19 00:05:26 -0400

    nothing

--- PoliceRecorder/PoliceRecorder/CameraService.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CameraService.swift
-//  PoliceRecorder
-//
-//  Created by Pranav on 9/18/21.
-//
-
-import Foundation
-import SwiftUI
---
+++ PoliceRecorder/PoliceRecorder/CameraView.swift
@@ -0,0 +1,15 @@
+//
+//  CameraView.swift
+//  PoliceRecorder
+//
+//  Created by Pranav on 9/18/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct CameraView: View{
+    var body: some View{
+        Text("Test")
+    }
+}
